f019d5cd921a8adf113b61ae35baad6dfd322f81
jmaytona <john.m.m.aytona@accenture.com>
2021-07-21 13:38:34 +0800

    2nd commit

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
-    std::cout << "Hello, World!";
+    std::cout << "You are breaking into J&A to steal Jun's HDD...";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to get into the building undetected...";
+    std::cout << std::endl;
+
+    int a = 5;
+    int b = 2;
+    int c = 3;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
