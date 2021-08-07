a2be4dc363d5de74286d5a19cdc3880e89fcbc0a
jmaytona <mjaytona@gmail.com>
2021-08-07 18:10:19 +0800

    4th commit

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // Print opening messages to the terminal
     std::cout << "You are breaking into J&A to steal Jun's HDD...";
     std::cout << std::endl;
     std::cout << "It seems to locked behind the walls of J&A...";
@@ -9,13 +10,21 @@ int main()
     std::cout << "You need to enter the correct codes to get into the building undetected...";
     std::cout << std::endl;
 
-    int a = 5;
-    int b = 2;
-    int c = 3;
+    // Declare 3 number code for level 1
+    const int a = 5;
+    const int b = 2;
+    const int c = 3;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    /* THIS IS
+        A MULTI-LINE
+        COMMENT...
+        DELETE FOR NEXT VIDEO
+    */
 
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    // Print sum and product to terminal
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
 
