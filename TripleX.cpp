f94cff68cb1832536a6af1c48b9f6b64bfabd362
jmaytona <mjaytona@gmail.com>
2021-08-16 17:57:53 +0800

    13th commit

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,12 +3,12 @@
 void PrintIntroduction()
 {
     // Print opening messages to the terminal
-    std::cout << "You are breaking into J&A to steal Matsumoto Jun's HDD...\n";
+    std::cout << "\nYou are breaking into J&A to steal Matsumoto Jun's HDD...\n";
     std::cout << "It seems to be locked behind the walls of J&A...\n";
     std::cout << "You need to enter the correct codes to get into the building undetected...\n\n";
 }
 
-void PlayGame()
+bool PlayGame()
 {
     PrintIntroduction();
 
@@ -41,19 +41,26 @@ void PlayGame()
         std::cout << "The security measures for the whole lobby has been deactivated...\n";
         std::cout << "The back-door opened...\n";
         std::cout << "You head straight to the elevator to Basement 2 and found Jun'ss HDD.\n";
-        std::cout << "YOU WIN!";
+        std::cout << "YOU WIN!\n";
+        return true;
     } 
     else
     {
         std::cout << "\n\nYour tripped the alarm!\n";
         std::cout << "The guards caught you...\n";
         std::cout << "GAME OVER\n";
+        return false;
     }
 }
 
 int main()
 {
-    
-    PlayGame();
+    while (true)
+    {
+        bool bLevelComplete = PlayGame();
+        std::cin.clear();   // Clears any errors
+        std::cin.ignore();  // Discards the buffer
+    }
+
     return 0;
 }
